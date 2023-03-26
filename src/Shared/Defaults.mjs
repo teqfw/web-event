40e0630e299e11fe53bc6f051558deaa9969ac54
Alex Gusev <alex@flancer64.com>
2023-03-26 11:29:52 +0300

    UP01-19 Improve app startup after installation

--- src/Shared/Defaults.mjs
+++ src/Shared/Defaults.mjs
@@ -7,9 +7,9 @@ export default class TeqFw_Web_Event_Shared_Defaults {
 
     // URL prefix for API requests: https://.../door/space/...
 
-    SPACE_DIRECT = 'web-event-direct'; // events stream from front to back
-    SPACE_FRONT_REG = 'web-event-front-reg'; // web events: front registration
-    SPACE_STREAM_ACTIVATE = 'web-event-stream-act'; // web events: activate the reverse stream
+    SPACE_DIRECT = 'web-event-direct'; // events stream from front to back (POST)
+    SPACE_FRONT_REG = 'web-event-front-reg'; // web events: front registration (POST)
+    SPACE_STREAM_ACTIVATE = 'web-event-stream-act'; // web events: activate the reverse stream (POST)
     SPACE_STREAM_OPEN = 'web-event-stream-open'; // web events: open the reverse stream
 
     SSE_AUTHENTICATE = 'authenticate'; // SSE event name
