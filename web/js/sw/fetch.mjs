97a913301bd55cee0e6caf24c5723f9f618fc8ba
Alex Gusev <alex@flancer64.com>
2024-01-07 13:34:11 +0200

    SSK-109 Use zipped sources in the SW #53

---
+++ web/js/sw/fetch.mjs
@@ -0,0 +1,17 @@
+/**
+ * Module with functions to use in a service worker during the fetch stage.
+ */
+// MODULE'S IMPORTS
+
+// MODULE'S VARS
+
+// MODULE'S FUNCS
+/**
+ * Function to detect URLs that always bypass caching for @teqfw/web-api plugin.
+ * @param {Request} req
+ * @return {boolean}
+ */
+export function bypassCache(req) {
+    const SSE_OPEN = /(.*)(\/web-event-stream-open\/)(.*)/; // GET request to open SSE channel
+    return Boolean(req.url.match(SSE_OPEN));
+}
